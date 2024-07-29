22a088c1a73cf056136ae24038ba3cad82a5170a
Farrukh <mfarrukhjon@gmail.com>
2024-07-29 22:55:45 +0500

    transparent false

--- lua/webstyle/plugins/colorscheme-kanagawa.lua
+++ lua/webstyle/plugins/colorscheme-kanagawa.lua
@@ -10,7 +10,7 @@ return {
       keywordStyle = { italic = true },
       statementStyle = { bold = true },
       typeStyle = {},
-      transparent = true, -- do not set background color
+      transparent = false, -- do not set background color
       dimInactive = false, -- dim inactive window `:h hl-NormalNC`
       terminalColors = true, -- define vim.g.terminal_color_{0,17}
       colors = { -- add/modify theme and palette colors
@@ -20,7 +20,7 @@ return {
       overrides = function(colors) -- add/modify highlights
         return {}
       end,
-      theme = "lotus", -- Load "wave" theme when 'background' option is not set
+      theme = "wave", -- Load "wave" theme when 'background' option is not set
       background = { -- map the value of 'background' option to a theme
         dark = "wave", -- try "dragon" !
         light = "lotus",
