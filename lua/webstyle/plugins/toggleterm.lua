d57ff0c78dad9928498d15237f44bdbeb281756d
farrukh <mfarrukhjon@gmail.com>
2024-08-01 18:26:53 +0500

    configs changed

--- lua/webstyle/plugins/toggleterm.lua
+++ lua/webstyle/plugins/toggleterm.lua
@@ -1,4 +1,14 @@
 return {
   -- amongst your other plugins
-  { "akinsho/toggleterm.nvim", version = "*", config = true },
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = true,
+    opts = {
+      open_mapping = [[<c-\>]],
+      float_opts = {
+        border = "curved",
+      },
+    },
+  },
 }
